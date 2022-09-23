b98f7a4b5fce9a4b4561294cd9fa7fd6e6bf6e61
USABash <abozuriaqbashar@gmail.com>
2022-09-22 19:30:13 -0500

    job

---
+++ new.js
@@ -0,0 +1,50 @@
+console.log("hello world");
+console.log(5);
+console.log(5 + 5)
+let name = 'iyas'; 
+let age = 26;
+let bash = "love";
+console.log(name);
+console.log(age + 30) 
+console.log("my name is " + name + " and I'm " + age + " years old"); // concatenation 
+console.log(`my name is ${name} and I'm ${age} years old`); // template literals ake 3sfora
+
+let meal = "lunch";
+const food = "mansaf"; 
+console.log(`${name} had ${food} for ${meal}`); 
+
+console.log(`${name} had ${food} for ${meal}`); 
+
+let num1 = 4;
+let num2 = 10;
+num1 = num1 + 2; 
+num1 += 2;
+num2 = num2 - 2;
+console.log(num1);
+num2 -= 2; 
+console.log(num2);
+
+num1 = num1 + 1 ;
+num1 += 1 ;
+num1++; 
+
+num2 = num2 - 1;
+num2 -= 1;
+num2--;
+console.log(num1);
+console.log(num2);
+console.log("stop");
+console.log(num1 + 4);
+console.log(num1++);
+console.log(num1);
+console.log(++num1);
+console.log(num1);
+console.log("stop");
+
+console.log(num1+4);
+console.log(num1);
+
+
+
+
+
